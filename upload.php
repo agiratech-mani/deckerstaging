85e244ce6d549bad83a77b370847503df937cca4
Manikandan <mani@agiratech.com>
2017-03-27 13:40:57 +0530

    Changes

--- upload.php
+++ upload.php
@@ -1,13 +1,20 @@
 <?php
 if(!empty($_POST['datauri'])){
     $basefolder = $_POST['basefolder'];
-     if(!is_dir($basefolder)){
+    $filename = $_POST['filename'];
+    $folder = $basefolder.$filename;
+    if(file_exists($folder))
+    {
+        unlink($folder);     
+    }
+    if(!is_dir($basefolder))
+    {
     //Directory does not exist, so lets create it.
         mkdir($basefolder, 0777, true);
     }
 
-    $filename = $_POST['filename'];
-    $folder = $basefolder.$filename;
+    
+    //$folder = $basefolder.$filename;
     $filepath =  __DIR__.'/'.$folder;
     $encoded_data = $_POST['datauri'];
     $encoded_data = str_replace('data:application/pdf;base64,', '', $encoded_data);

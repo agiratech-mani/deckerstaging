43cfd6a8215d7e5af9ba714d488c8768e4d81333
Manikandan <mani@agiratech.com>
2017-05-31 15:30:23 +0530

    Fixes

---
+++ sample/test.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: application/octet-stream');
+// This "fakes" the file name, so the downloaded file isn't called
+// "download_xml_file.php" or whatever you name the script.
+header('Content-Disposition: attachment; filename=pdf.pdf');
+readfile('http://www.pdf995.com/samples/pdf.pdf');
+
+?>

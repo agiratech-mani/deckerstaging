3110077795b1ae73997f5e9e0cd4e931bd4fa942
mani <mani@agiratech.com>
2017-11-16 20:41:05 +0530

    Final Build

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+	//int_set("display_errors",1);
+	//require_once("vendor/autoload.php");
+	/*use BoyHagemann\Wave\Wave;
+	$wave = new Wave();
+	$wave->setFilename('flanger.wav');
+	$amplitudes = [];
+	$data = $wave->getWaveformData();
+	// Get the amplitude values for each channel
+	foreach($data->getChannels() as $channel) {
+	    $amplitudes = $channel->getValues();
+	}*/
+	/*$metadata = $wave->analyze()->getMetadata();
+	echo "Name:".$metadata->getName();
+	echo "getSize:".$metadata->getSize();
+	echo "getFormat:".$metadata->getFormat();
+	echo "getChannels:".$metadata->getChannels();
+	echo "getSampleRate:".$metadata->getSampleRate();
+	echo "getBytesPerSecond:".$metadata->getBytesPerSecond();
+	echo "getBlockSize:".$metadata->getBlockSize();
+	echo "getBitsPerSample:".$metadata->getBitsPerSample();
+	echo "getExtensionSize:".$metadata->getExtensionSize();
+	echo "getExtensionData:".$metadata->getExtensionData();*/
+
+	//echo "<pre>";print_r($amplitudes);echo "</pre>";
+	phpinfo();
+	// use BoyHagemann\Waveform\Waveform;
+	// use BoyHagemann\Waveform\Generator;
+
+	// $filename = 'flanger.wav';
+
+	// $waveform =  Waveform::fromFilename($filename);
+	// $waveform->setGenerator(new Generator\Png)
+	// ->setWidth(960)
+	// ->setHeight(400);
+
+	// // Will display the image, including setting the read image/png header
+	// echo $waveform->generate();
+?>
